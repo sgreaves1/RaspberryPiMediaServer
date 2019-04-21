633acc32425f6d65543a37eac405b65a1fa05edb
Sam Greaves <samgreaves0@gmail.com>
2019-04-21 09:19:51 +0100

    PM-4 add react front end

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        console.log('render');
+
+        return (
+            <div className="App">
+                hello
+            </div>
+        );
+    }
+}
+
+export default App;
