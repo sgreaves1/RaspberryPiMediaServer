c2d5a9440a3feb1894bbbeffd7bb178bd312a09b
Sam Greaves <samgreaves0@gmail.com>
2019-04-22 14:40:32 +0100

    PM-4 remove unused index

--- src/Components/Content/ListOfFilms/ListOfFilms.js
+++ src/Components/Content/ListOfFilms/ListOfFilms.js
@@ -6,7 +6,7 @@ export class ListOfFilms extends React.Component {
     render() {
 
         const items = [];
-        for (const [index, value] of this.props.films.entries()) {
+        for (const [value] of this.props.films.entries()) {
             items.push(<div class="film-box">
                 {value}
             </div>)
