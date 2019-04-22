cc59ddd6eb0fe956da77a5c4f7bb94c0c002443f
Sam Greaves <samgreaves0@gmail.com>
2019-04-22 14:23:50 +0100

    PM-4 display each film name

--- src/Components/Content/ListOfFilms/ListOfFilms.js
+++ src/Components/Content/ListOfFilms/ListOfFilms.js
@@ -2,10 +2,19 @@ import React from "react";
 import './ListOfFilms.css';
 
 export class ListOfFilms extends React.Component {
+
     render() {
+
+        const items = [];
+        for (const [index, value] of this.props.films.entries()) {
+            items.push(<div class="film-box">
+                {value}
+            </div>)
+        }
+
         return (
             <div class="list-films-box">
-                Hey
+                {value}
             </div>
         )
     }
