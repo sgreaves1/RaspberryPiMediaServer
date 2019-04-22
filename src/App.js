20693cfc7c8362c8c3b555e1eefb56799af4d994
Sam Greaves <samgreaves0@gmail.com>
2019-04-22 13:32:57 +0100

    PM-4 get list of films in react front end

--- src/App.js
+++ src/App.js
@@ -2,8 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            films: [
+                {
+                    "file": null
+                }
+            ]
+        };
+    }
+
+    componentDidMount() {
+        this.getFilms();
+    }
+
+    getFilms() {
+        fetch('videos/')
+            .then(res => res.json())
+            .then(json => {this.setState({films: json})});
+    }
+
     render() {
-        console.log('render');
+        console.log(films);
 
         return (
             <div className="App">
---
+++ src/Components/Content/ListOfFilms/ListOfFilms.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Content.css';
+
+export class Content extends React.Component {
+    render() {
+        <div class="list-films-box">
+            Hey
+        </div>
+    }
+}
