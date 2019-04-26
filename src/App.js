1ee5b6239ae46094d12ff8d3a4132df3c8290308
Sam Greaves <samgreaves0@gmail.com>
2019-04-26 19:33:29 +0100

    Revert "PM-9 log results to see if any videos returned"
    This reverts commit e6be241e8d40e4cb00b395f613609fe57887645a.

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ class App extends Component {
         if (video.Type === "movie") {
             await fetch('https://api.themoviedb.org/3/movie/'+ video.imdbID +'/videos?api_key=' + movieDBKey)
                 .then(res => res.json())
-                .then(json => {console.log(json)});
+                .then(json => {video.youtubeKey = json.results[0].key});
 
             films.push(video);
         }
