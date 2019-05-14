75beb9a2088954d0eca32677709e381d93ca0c6c
Sam Greaves <samgreaves0@gmail.com>
2019-05-14 21:46:42 +0100

    PM-14 try absolute url

--- src/Components/VideoPlayer.js
+++ src/Components/VideoPlayer.js
@@ -3,7 +3,7 @@ import './VideoPlayer.css';
 
 export class VideoPlayer extends Component {
     render() {
-        let videoUrl = '/videos/' + this.props.video.imdbID + this.props.video.videoFormat;
+        let videoUrl = 'http://192.168.1.18:3020/videos/' + this.props.video.imdbID + this.props.video.videoFormat;
         let video = <iframe src={videoUrl} allowFullScreen='true' className="videoPlayer"/>;
 
         return (
