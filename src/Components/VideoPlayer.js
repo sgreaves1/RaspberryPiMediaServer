5506bf35b9f982082ed850e196aae2a5141a191e
Sam Greaves <samgreaves0@gmail.com>
2019-05-14 21:23:02 +0100

    PM-14 remove localhost from video url

--- src/Components/VideoPlayer.js
+++ src/Components/VideoPlayer.js
@@ -3,8 +3,7 @@ import './VideoPlayer.css';
 
 export class VideoPlayer extends Component {
     render() {
-        let videoUrl = 'http://localhost:3020/videos/' + this.props.video.imdbID + this.props.video.videoFormat;
-        console.log(videoUrl);
+        let videoUrl = '/videos/' + this.props.video.imdbID + this.props.video.videoFormat;
         let video = <iframe src={videoUrl} allowFullScreen='true' className="videoPlayer"/>;
 
         return (
