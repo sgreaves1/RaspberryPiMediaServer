50b56b8afcf3edca57a30e4871ef0602d18e25a8
sgreaves1 <samgreaves0@gmail.com>
2019-10-23 23:42:07 +0100

    think i fixed it

--- src/Helpers/movieApis.js
+++ src/Helpers/movieApis.js
@@ -90,9 +90,9 @@ async function SetHaveEpisode(show, seasonNumber, episodeNumber) {
     for (let i = 0; i < show.seasons.length; i++) {
         if (show.seasons[i].number === seasonNumber) {
             for (let j = 0; j < show.seasons[i].episodes.length; j++) {
-                console.log(show.seasons[i].episodes[i]);
-                if (show.seasons[i].episodes[i].episode_number === episodeNumber) {
-                    show.seasons[i].episodes[i].enabled = true;
+                console.log(show.seasons[i].episodes[j]);
+                if (show.seasons[i].episodes[j].episode_number === episodeNumber) {
+                    show.seasons[i].episodes[j].enabled = true;
                     return;
                 }
             }
