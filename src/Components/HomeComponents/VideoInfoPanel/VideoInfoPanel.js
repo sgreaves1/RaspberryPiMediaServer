7d657dd71ccb9a958aaeef1bc1e591de60cf4f81
sgreaves1 <samgreaves0@gmail.com>
2019-11-21 13:54:36 +0000

    Make backdrop transparent

--- src/Components/HomeComponents/VideoInfoPanel/VideoInfoPanel.js
+++ src/Components/HomeComponents/VideoInfoPanel/VideoInfoPanel.js
@@ -38,7 +38,8 @@ export class VideoInfoPanel extends React.Component {
 
         if (this.props.selectedVideo.Images) {
             divStyle = {
-                backgroundImage: 'url(https://image.tmdb.org/t/p/original/'+ this.props.selectedVideo.Images.backdrops[0].file_path +')',
+                marginTop: '10px',
+                backgroundImage: `linear-gradient( to right, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.2)),url(https://image.tmdb.org/t/p/original/${this.props.selectedVideo.Images.backdrops[0].file_path})`,
                 backgroundSize: '110vh',
                 backgroundRepeat: 'no-repeat',
                 backgroundPosition: 'right',
