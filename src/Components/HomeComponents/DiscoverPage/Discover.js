18b35ff3c346472c1fc2bf9a86458e52aada2cc0
sgreaves1 <samgreaves0@gmail.com>
2019-11-29 14:23:20 +0000

    Cleanup discover page

--- src/Components/HomeComponents/DiscoverPage/Discover.js
+++ src/Components/HomeComponents/DiscoverPage/Discover.js
@@ -21,46 +21,12 @@ export class Discover extends React.Component {
 
     render() {
 
-        let videosToShowRow1 = [];
-        let videosToShowRow2 = [];
         let blockOfVideos;
 
         if (this.state.videos && this.state.videos.results && this.state.videos.results.length > 3) {
-            videosToShowRow1.push(this.state.videos.results[0]);
-            videosToShowRow1.push(this.state.videos.results[1]);
-            videosToShowRow1.push(this.state.videos.results[2]);
-
-            videosToShowRow2.push(this.state.videos.results[0]);
-            videosToShowRow2.push(this.state.videos.results[1]);
-            videosToShowRow2.push(this.state.videos.results[2]);
-            videosToShowRow2.push(this.state.videos.results[3]);
-            videosToShowRow2.push(this.state.videos.results[4]);
-            videosToShowRow2.push(this.state.videos.results[5]);
-            videosToShowRow2.push(this.state.videos.results[6]);
-            videosToShowRow2.push(this.state.videos.results[7]);
-            videosToShowRow2.push(this.state.videos.results[8]);
-
-
-            let image1Url = `https://image.tmdb.org/t/p/original/${videosToShowRow2[0].backdrop_path}`;
-            let image2Url = `https://image.tmdb.org/t/p/original/${videosToShowRow2[1].backdrop_path}`;
-            let image3Url = `https://image.tmdb.org/t/p/original/${videosToShowRow2[2].backdrop_path}`;
-            let image4Url = `https://image.tmdb.org/t/p/original/${videosToShowRow2[3].backdrop_path}`;
-            let image5Url = `https://image.tmdb.org/t/p/original/${videosToShowRow2[4].backdrop_path}`;
-            let image6Url = `https://image.tmdb.org/t/p/original/${videosToShowRow2[5].backdrop_path}`;
-            let image7Url = `https://image.tmdb.org/t/p/original/${videosToShowRow2[6].backdrop_path}`;
-            let image8Url = `https://image.tmdb.org/t/p/original/${videosToShowRow2[7].backdrop_path}`;
-            let image9Url = `https://image.tmdb.org/t/p/original/${videosToShowRow2[8].backdrop_path}`;
-
-            let divStyle = {
-                display: 'inline-block',
-                width: 'auto',
-                height: 'auto',
-                margin: 0,
-                padding: 0,
-            };
 
             let background1 = {
-                backgroundImage: `url(https://image.tmdb.org/t/p/original/${videosToShowRow2[0].backdrop_path})`,
+                backgroundImage: `url(https://image.tmdb.org/t/p/original/${this.state.videos.results[0].backdrop_path})`,
                 backgroundSize: '100%',
                 backgroundRepeat: 'no-repeat',
                 backgroundPosition: 'right',
@@ -68,7 +34,7 @@ export class Discover extends React.Component {
             };
 
             let background2 = {
-                backgroundImage: `url(https://image.tmdb.org/t/p/original/${videosToShowRow2[1].backdrop_path})`,
+                backgroundImage: `url(https://image.tmdb.org/t/p/original/${this.state.videos.results[1].backdrop_path})`,
                 backgroundSize: '100%',
                 backgroundRepeat: 'no-repeat',
                 backgroundPosition: 'right',
@@ -76,7 +42,7 @@ export class Discover extends React.Component {
             };
 
             let background3 = {
-                backgroundImage: `url(https://image.tmdb.org/t/p/original/${videosToShowRow2[2].backdrop_path})`,
+                backgroundImage: `url(https://image.tmdb.org/t/p/original/${this.state.videos.results[2].backdrop_path})`,
                 backgroundSize: '100%',
                 backgroundRepeat: 'no-repeat',
                 backgroundPosition: 'right',
@@ -84,7 +50,7 @@ export class Discover extends React.Component {
             };
 
             let background4 = {
-                backgroundImage: `url(https://image.tmdb.org/t/p/original/${videosToShowRow2[3].backdrop_path})`,
+                backgroundImage: `url(https://image.tmdb.org/t/p/original/${this.state.videos.results[3].backdrop_path})`,
                 backgroundSize: '100%',
                 backgroundRepeat: 'no-repeat',
                 backgroundPosition: 'right',
@@ -92,7 +58,7 @@ export class Discover extends React.Component {
             };
 
             let background5 = {
-                backgroundImage: `url(https://image.tmdb.org/t/p/original/${videosToShowRow2[4].backdrop_path})`,
+                backgroundImage: `url(https://image.tmdb.org/t/p/original/${this.state.videos.results[4].backdrop_path})`,
                 backgroundSize: '100%',
                 backgroundRepeat: 'no-repeat',
                 backgroundPosition: 'right',
@@ -100,7 +66,7 @@ export class Discover extends React.Component {
             };
 
             let background6 = {
-                backgroundImage: `url(https://image.tmdb.org/t/p/original/${videosToShowRow2[5].backdrop_path})`,
+                backgroundImage: `url(https://image.tmdb.org/t/p/original/${this.state.videos.results[5].backdrop_path})`,
                 backgroundSize: '100%',
                 backgroundRepeat: 'no-repeat',
                 backgroundPosition: 'right',
