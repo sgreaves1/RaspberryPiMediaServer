b3b06e1a10a01a49c5ddcb9b70bccb65249bd0a0
sam greaves <sam.greaves@bglgroup.com>
2020-05-30 01:15:09 +0100

    add more images

--- src/Components/HomeComponents/TestLayoutPanel/TestLayoutPanel.js
+++ src/Components/HomeComponents/TestLayoutPanel/TestLayoutPanel.js
@@ -50,46 +50,56 @@ export class TestLayoutPanel extends React.Component {
 
                 video1 = this.props.videos[this.state.starting + 1];
                 if (video1) {
-                    let imageSource = video1.backdrop_path ? video1.backdrop_path : video1.poster_path;
+                    let backupImageSource = video1.backdrop_path ? video1.backdrop_path : video1.poster_path;
+                    let test = "this.src='https://image.tmdb.org/t/p/original/" + backupImageSource +"'";
+                    let imageSource = this.getImage(video1);
                     items.push(<div key={video1.title} className="film-backdrop-box">
                         <img data-tip={`<center>${video1.title}</center>`} className="film-backdrop"
                              alt={`${video1.title} poster`}
-                             src={`https://image.tmdb.org/t/p/original/${imageSource}`}/>
+                             src={imageSource}
+                             onError={this.addDefaultSrc.bind(this, backupImageSource)}/>
                         <ReactTooltip html={true}/>
                     </div>);
                 }
 
                 video1 = this.props.videos[this.state.starting + 2];
                 if (video1) {
-                    let imageSource = video1.backdrop_path ? video1.backdrop_path : video1.poster_path;
+                    let backupImageSource = video1.backdrop_path ? video1.backdrop_path : video1.poster_path;
+                    let test = "this.src='https://image.tmdb.org/t/p/original/" + backupImageSource +"'";
+                    let imageSource = this.getImage(video1);
                     items.push(<div key={video1.title} className="film-backdrop-box">
                         <img data-tip={`<center>${video1.title}</center>`} className="film-backdrop"
                              alt={`${video1.title} poster`}
-                             src={`https://image.tmdb.org/t/p/original/${imageSource}`}/>
+                             src={imageSource}
+                             onError={this.addDefaultSrc.bind(this, backupImageSource)}/>
                         <ReactTooltip html={true}/>
                     </div>);
                 }
 
                 video1 = this.props.videos[this.state.starting + 3];
                 if (video1) {
-                    let imageSource = video1.backdrop_path ? video1.backdrop_path : video1.poster_path;
+                    let backupImageSource = video1.backdrop_path ? video1.backdrop_path : video1.poster_path;
+                    let test = "this.src='https://image.tmdb.org/t/p/original/" + backupImageSource +"'";
+                    let imageSource = this.getImage(video1);
                     items.push(<div key={video1.title} className="film-backdrop-box">
                         <img data-tip={`<center>${video1.title}</center>`} className="film-backdrop"
                              alt={`${video1.title} poster`}
-                             src={`https://image.tmdb.org/t/p/original/${imageSource}`}/>
+                             src={imageSource}
+                             onError={this.addDefaultSrc.bind(this, backupImageSource)}/>
                         <ReactTooltip html={true}/>
                     </div>);
                 }
 
                 video1 = this.props.videos[this.state.starting + 4];
                 if (video1) {
-                    let imageSource = video1.backdrop_path ? video1.backdrop_path : video1.poster_path;
-
-
+                    let backupImageSource = video1.backdrop_path ? video1.backdrop_path : video1.poster_path;
+                    let test = "this.src='https://image.tmdb.org/t/p/original/" + backupImageSource +"'";
+                    let imageSource = this.getImage(video1);
                     items.push(<div key={video1.title} className="film-backdrop-box">
                         <img data-tip={`<center>${video1.title}</center>`} className="film-backdrop"
                              alt={`${video1.title} poster`}
-                             src={`https://image.tmdb.org/t/p/original/${imageSource}`}/>
+                             src={imageSource}
+                             onError={this.addDefaultSrc.bind(this, backupImageSource)}/>
                         <ReactTooltip html={true}/>
                     </div>);
                 }
