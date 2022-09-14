da2227a9df47e5b6c4dca86dfdd6726cbe3d2240
Sam Greaves <sam.greaves@bglgroup.co.uk>
2022-09-14 18:06:22 +0100

    Add movies (trailers) route

--- movieStub/app.js
+++ movieStub/app.js
@@ -5,6 +5,7 @@ let app = express();
 
 const findRoute = require('./routes/find');
 const omdbRoute = require('./routes/omdb');
+const movieRoute = require('./routes/movie'); // Gets videos (i.e trailers ect)
 
 app.use(express['static'](__dirname ));
 
@@ -13,6 +14,7 @@ app.use(bodyParser.json());
 
 app.use('/', omdbRoute);
 app.use('/find', findRoute);
+app.use('/movie', movieRoute);
 
 app.listen(65535);
 
