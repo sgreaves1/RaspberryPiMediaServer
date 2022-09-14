65b58a6f6eac469b5e0a6aeff61e4c380f84cd36
Sam Greaves <sam.greaves@bglgroup.co.uk>
2022-09-14 18:46:23 +0100

    add tv shows endpoint

--- movieStub/app.js
+++ movieStub/app.js
@@ -6,6 +6,7 @@ let app = express();
 const findRoute = require('./routes/find');
 const omdbRoute = require('./routes/omdb');
 const movieRoute = require('./routes/movie'); // Gets videos (i.e trailers ect)
+const tvRoute = require('./routes/tv');
 
 app.use(express['static'](__dirname ));
 
@@ -15,6 +16,7 @@ app.use(bodyParser.json());
 app.use('/', omdbRoute);
 app.use('/find', findRoute);
 app.use('/movie', movieRoute);
+app.use('/tv', tvRoute);
 
 app.listen(65535);
 
